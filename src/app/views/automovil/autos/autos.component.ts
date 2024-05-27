c788104a0a8cc2da56eb652f3b8b9e61a94a020e
Cacho78 <147776832+Cacho78@users.noreply.github.com>
2024-05-27 19:12:00 -0400

    terminando maquetacion

--- src/app/views/automovil/autos/autos.component.ts
+++ src/app/views/automovil/autos/autos.component.ts
@@ -1,12 +1,18 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RowComponent, ColComponent, CardComponent, CardBodyComponent, CardHeaderComponent, FormControlDirective, FormDirective, FormLabelDirective, ButtonDirective, TextColorDirective, FormSelectDirective, TableActiveDirective, TableColorDirective, TableDirective } from '@coreui/angular';
+
 
 @Component({
   selector: 'app-autos',
   standalone: true,
-  imports: [],
-  templateUrl: './autos.component.html',
+  imports: [RowComponent, ColComponent, CardComponent, CardHeaderComponent, FormControlDirective,
+    CardBodyComponent, ReactiveFormsModule, FormSelectDirective,FormsModule, FormDirective,
+     FormLabelDirective,TableDirective, TableColorDirective, TableActiveDirective, FormControlDirective,ButtonDirective,TextColorDirective,NgStyle],
+    templateUrl: './autos.component.html',
   styleUrl: './autos.component.scss'
-})
+  })
 export class AutosComponent {
 
 }
--- src/app/views/automovil/reportes/reportes.component.ts
+++ src/app/views/automovil/reportes/reportes.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RowComponent, ColComponent, CardComponent, CardHeaderComponent, CardBodyComponent } from '@coreui/angular';
 
 @Component({
   selector: 'app-reportes',
   standalone: true,
-  imports: [],
+  imports: [RowComponent, ColComponent, CardComponent, CardHeaderComponent,CardBodyComponent],
   templateUrl: './reportes.component.html',
   styleUrl: './reportes.component.scss'
 })
